7b30791dc46989ff3ccf2ee28d3df1e809fefdae
Carol <redrumjedi@gmail.com>
2020-09-13 14:34:11 -0300

    removes typescript preset - no need in the new version

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -1,8 +1,8 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
   addons: [
-    '@storybook/preset-typescript',
     '@storybook/addon-links',
-    '@storybook/addon-essentials'
+    '@storybook/addon-essentials',
+    '@storybook/addon-actions'
   ]
 }
